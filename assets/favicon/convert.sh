bc8081aa3fe93f81bc4fad457811291eb9938e8b
s0urce-c0de <email.kanavg@gmail.com>
2024-12-09 03:09:16 +0000

    mistak

--- assets/favicon/convert.sh
+++ assets/favicon/convert.sh
@@ -1,9 +1,11 @@
 # Install inkscape and image.... to do this
 
-ROOT=$(dirname "$(dirname "$(dirname "$(realpath "$0")")")")
+PARDIR="$(dirname "$(realpath "$0")")"
+
+cd $PARDIR
 
 export sizes=({16..256})
 convert -background transparent -size 1024x1024 favicon.svg favicon.png
 convert -background transparent -define "icon:auto-resize=$(IFS=","; echo "${array[*]}")" favicon.svg favicon.ico
 
-cp favicon.ico $ROOT
+cp favicon.ico ../../
