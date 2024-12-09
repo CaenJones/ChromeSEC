fbd269f91a639d3e5affad8d6f032f2c82cd9943
s0urce-c0de <email.kanavg@gmail.com>
2024-12-09 03:13:17 +0000

    github pages is f*cking itself

--- assets/favicon/convert.sh
+++ assets/favicon/convert.sh
@@ -8,4 +8,4 @@ export sizes=({16..256})
 convert -background transparent -size 1024x1024 favicon.svg favicon.png
 convert -background transparent -define "icon:auto-resize=$(IFS=","; echo "${array[*]}")" favicon.svg favicon.ico
 
-cp favicon.ico ../../
+cp favicon.ico ../../ 
